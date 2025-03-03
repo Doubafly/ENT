456011dc170d41d6dbb50f6b0a8d8414d3b9e40d
Dayif <sekoudayifourouk@gmail.com>
2025-03-03 13:06:34 +0000

    mis à jour

--- components/list/EnseignantList.tsx
+++ components/list/EnseignantList.tsx
@@ -1,39 +1,126 @@
 "use client";
 
-import { useState } from "react";
 import ListCard, { User } from "@/components/card/ListCard";
 import Modal from "@/components/modal/Modal";
+import { useState } from "react";
 
 const enseignantsData: User[] = [
-  { image: "/img/profil1.jpg", nom: "Dupont", prenom: "Jean", email: "mah@example.com", adresse: "Paris, France", date: "01/01/1980", tel: "0123456789" },
-  { image: "/img/profil2.jpg", nom: "Dupont", prenom: "Jean", email: "kissa@example.com", adresse: "Paris, France", date: "01/01/1980", tel: "0123456789" },
-  { image: "/img/profil3.jpg", nom: "Dupont", prenom: "Jean", email: "awa@example.com", adresse: "Paris, France", date: "01/01/1980", tel: "0123456789" },
-  { image: "/img/profil4.jpg", nom: "Dupont", prenom: "Jean", email: "dramane@example.com", adresse: "Paris, France", date: "01/01/1980", tel: "0123456789" },
-  { image: "/img/profil5.jpg", nom: "Dupont", prenom: "Jean", email: "moussa@example.com", adresse: "Paris, France", date: "01/01/1980", tel: "0123456789" },
-  { image: "/img/profil6.jpg", nom: "Dupont", prenom: "Jean", email: "issa@example.com", adresse: "Paris, France", date: "01/01/1980", tel: "0123456789" },
-  { image: "/img/profil6.jpg", nom: "Dupont", prenom: "Jean", email: "issa@example.com", adresse: "Paris, France", date: "01/01/1980", tel: "0123456789" },
-  { image: "/img/profil7.jpg", nom: "Dupont", prenom: "Jean", email: "bourma@example.com", adresse: "Paris, France", date: "01/01/1980", tel: "0123456789" },
-  { image: "/img/profil8.jpg", nom: "Maiga", prenom: "Mahamoud", email: "bourma@example.com", adresse: "Bamako, Sotuba", date: "01/01/1980", tel: "0123456789" },
-  { image: "/img/profil9.jpg", nom: "DIALLO", prenom: "Moussa", email: "bourma@example.com", adresse: "Bamako, Mali", date: "01/01/1980", tel: "0123456789" },
+  {
+    image: "/img/profil1.jpg",
+    nom: "Dupont",
+    prenom: "Jean",
+    email: "mah@example.com",
+    adresse: "Paris, France",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
+  {
+    image: "/img/profil2.jpg",
+    nom: "Dupont",
+    prenom: "Jean",
+    email: "kissa@example.com",
+    adresse: "Paris, France",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
+  {
+    image: "/img/profil3.jpg",
+    nom: "Dupont",
+    prenom: "Jean",
+    email: "awa@example.com",
+    adresse: "Paris, France",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
+  {
+    image: "/img/profil4.jpg",
+    nom: "Dupont",
+    prenom: "Jean",
+    email: "dramane@example.com",
+    adresse: "Paris, France",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
+  {
+    image: "/img/profil5.jpg",
+    nom: "Dupont",
+    prenom: "Jean",
+    email: "moussa@example.com",
+    adresse: "Paris, France",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
+  {
+    image: "/img/profil6.jpg",
+    nom: "Dupont",
+    prenom: "Jean",
+    email: "issa@example.com",
+    adresse: "Paris, France",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
+  {
+    image: "/img/profil6.jpg",
+    nom: "Dupont",
+    prenom: "Jean",
+    email: "issa@example.com",
+    adresse: "Paris, France",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
+  {
+    image: "/img/profil7.jpg",
+    nom: "Dupont",
+    prenom: "Jean",
+    email: "bourma@example.com",
+    adresse: "Paris, France",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
+  {
+    image: "/img/profil8.jpg",
+    nom: "Maiga",
+    prenom: "Mahamoud",
+    email: "bourma@example.com",
+    adresse: "Bamako, Sotuba",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
+  {
+    image: "/img/profil9.jpg",
+    nom: "DIALLO",
+    prenom: "Moussa",
+    email: "bourma@example.com",
+    adresse: "Bamako, Mali",
+    date: "01/01/1980",
+    tel: "0123456789",
+  },
 ];
 
 export default function EnseignantList() {
   const [enseignants, setEnseignants] = useState<User[]>(enseignantsData);
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [selectedEnseigant, setSelectedEnseignant] = useState<User | null>(null); // État pour gérer l'affichage du modal
+  const [selectedEnseigant, setSelectedEnseignant] = useState<User | null>(
+    null
+  ); // État pour gérer l'affichage du modal
   const itemsPerPage = 8;
 
   // Filtrage des enseignants en fonction du terme de recherche
   const filteredEnseignants = enseignants.filter((enseignant) =>
-    `${enseignant.nom} ${enseignant.prenom} ${enseignant.email}`.toLowerCase().includes(searchTerm.toLowerCase())
+    `${enseignant.nom} ${enseignant.prenom} ${enseignant.email}`
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
   );
 
   // Calcul des pages
   const totalPages = Math.ceil(filteredEnseignants.length / itemsPerPage);
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentEnseignants = filteredEnseignants.slice(indexOfFirstItem, indexOfLastItem);
+  const currentEnseignants = filteredEnseignants.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
   // Gérer la pagination
   const handlePrevPage = () => {
@@ -46,7 +133,9 @@ export default function EnseignantList() {
 
   // Gérer la suppression d'un enseignant
   const handleDelete = (user: User) => {
-    const updatedEnseignants = enseignants.filter((e) => e.email !== user.email);
+    const updatedEnseignants = enseignants.filter(
+      (e) => e.email !== user.email
+    );
     // setEnseignants(updatedEnseignants);
     setEnseignants([...updatedEnseignants]);
 
@@ -59,7 +148,7 @@ export default function EnseignantList() {
   // console.log("Enseignants:", enseignants);
   // console.log("Current enseignants affichés:", currentEnseignants);
   // console.log(`Page actuelle : ${currentPage} / ${totalPages}`);
-  
+
   return (
     <div className="ml-0 px-1 py-5 text-xl">
       {/* Titre, barre de recherche et bouton Ajouter */}
@@ -82,44 +171,49 @@ export default function EnseignantList() {
 
       {/* Liste des enseignants */}
       <div className="flex justify-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-        {currentEnseignants.map((enseignant, index) => (
-  // <ListCard key={`${enseignant.email}-${index}`} item={enseignant} onEdit={() => {}} onDelete={handleDelete} />
-  <ListCard 
-  key={`${enseignant.email}-${index}`} 
-  item={enseignant} 
-  onEdit={() => {}} 
-  onDelete={handleDelete} 
-  onSelect={() => setSelectedEnseignant(enseignant)} 
-/>
-))}
-
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grig-cols-6 gap-6 p-6">
+          {currentEnseignants.map((enseignant, index) => (
+            // <ListCard key={`${enseignant.email}-${index}`} item={enseignant} onEdit={() => {}} onDelete={handleDelete} />
+            <ListCard
+              key={`${enseignant.email}-${index}`}
+              item={enseignant}
+              onEdit={() => {}}
+              onDelete={handleDelete}
+              onSelect={() => setSelectedEnseignant(enseignant)}
+            />
+          ))}
         </div>
       </div>
 
       {/* Pagination */}
       {totalPages > 1 && (
         <div className="flex justify-center mt-4 space-x-4">
-          <button onClick={handlePrevPage} disabled={currentPage === 1} className="px-4 py-2 bg-blue-500 text-white rounded-lg disabled:opacity-50">
+          <button
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}
+            className="px-4 py-2 bg-blue-500 text-white rounded-lg disabled:opacity-50"
+          >
             Précédent
           </button>
           <span className="text-lg font-semibold">
             Page {currentPage} sur {totalPages}
           </span>
-          <button onClick={handleNextPage} disabled={currentPage === totalPages} className="px-4 py-2 bg-blue-500 text-white rounded-lg disabled:opacity-50">
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+            className="px-4 py-2 bg-blue-500 text-white rounded-lg disabled:opacity-50"
+          >
             Suivant
           </button>
         </div>
       )}
-        {/* Modal */}
-            {selectedEnseigant && (
-          <Modal onClose={() => setSelectedEnseignant(null)}>
+      {/* Modal */}
+      {selectedEnseigant && (
+        <Modal onClose={() => setSelectedEnseignant(null)}>
           <div className="p-5 bg-white rounded-lg shadow-lg w-[400px] relative">
             {/* Bouton Fermer (en haut à droite) */}
-            <div className="absolute top-3 right-3">
-             
-            </div>
-      
+            <div className="absolute top-3 right-3"></div>
+
             {/* Section avec background gris */}
             <div className="bg-gray-100 p-5 rounded-lg flex flex-col items-center">
               {/* Image centrée */}
@@ -130,32 +224,44 @@ export default function EnseignantList() {
                   className="object-cover w-full h-full"
                 />
               </div>
-      
+
               {/* Nom et Email */}
               <h2 className="text-center text-lg font-bold mt-2">
                 {selectedEnseigant.nom} {selectedEnseigant.prenom}
               </h2>
               <p className="text-gray-500 text-sm">{selectedEnseigant.email}</p>
             </div>
-      
+
             {/* Contenu en colonne */}
             <div className="mt-4 flex justify-between items-start px-6">
               {/* Infos personnelles */}
               <div className="space-y-3 text-sm text-gray-700">
                 <p className="flex gap-2 items-center">
-                <img src="/icons/book.png" alt="Adresse" className="w-4 h-4" />
+                  <img
+                    src="/icons/book.png"
+                    alt="Adresse"
+                    className="w-4 h-4"
+                  />
                   {selectedEnseigant.adresse}
                 </p>
                 <p className="flex gap-2 items-center">
-                <img src="/icons/calendar.png" alt="Date" className="w-4 h-4" />
+                  <img
+                    src="/icons/calendar.png"
+                    alt="Date"
+                    className="w-4 h-4"
+                  />
                   {selectedEnseigant.date}
                 </p>
                 <p className="flex gap-2 items-center">
-                <img src="/icons/call.png" alt="Téléphone" className="w-4 h-4" />
+                  <img
+                    src="/icons/call.png"
+                    alt="Téléphone"
+                    className="w-4 h-4"
+                  />
                   {selectedEnseigant.tel}
                 </p>
               </div>
-      
+
               {/* Boutons à droite */}
               <div className="flex flex-col gap-2">
                 <button
@@ -164,7 +270,7 @@ export default function EnseignantList() {
                 >
                   Modifier
                 </button>
-      
+
                 <button
                   onClick={() => console.log("Supprimer", selectedEnseigant)}
                   className="bg-red-500 text-white px-4 py-1 text-sm rounded-lg hover:bg-red-600 transition-all duration-200"
@@ -172,18 +278,16 @@ export default function EnseignantList() {
                   Supprimer
                 </button>
                 <button
-                onClick={() => setSelectedEnseignant(null)}
-                className="bg-gray-300 text-gray-700 px-3 py-1 text-sm rounded-lg hover:bg-gray-400 transition-all duration-200"
-              >
-                Fermer
-              </button>
+                  onClick={() => setSelectedEnseignant(null)}
+                  className="bg-gray-300 text-gray-700 px-3 py-1 text-sm rounded-lg hover:bg-gray-400 transition-all duration-200"
+                >
+                  Fermer
+                </button>
               </div>
             </div>
           </div>
         </Modal>
       )}
-      
     </div>
-
   );
 }

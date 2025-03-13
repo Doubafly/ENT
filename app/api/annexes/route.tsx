d9a212957a39d33d780008cafd3f2ca895b0bd74
misskadidia149 <kd1807319@gmail.com>
2025-03-13 12:25:20 +0000

    commit sans but

--- app/api/annexes/route.tsx
+++ app/api/annexes/route.tsx
@@ -1,17 +1,4 @@
 import prisma from "../prisma";
-export async function GET() {
-    try {
-      const annexes = await prisma.annexes.findMany({
-      });
-      return new Response(JSON.stringify(annexes), { status: 200 });
-    } catch (e) {
-      return new Response(
-        JSON.stringify({ message: "Une erreur est survenue" }),
-        { status: 500 }
-      );
-    }
-  }
-import prisma from "../prisma";
 
 export async function GET() {
   try {

434acc03528d300e20dda897dc93c92e8ae91227
doubafly <ba6353158@gmail.com>
2025-04-25 08:18:03 +0000

    ajustement du login et des notes

--- app/(root)/etudiant/note/page.tsx
+++ app/(root)/etudiant/note/page.tsx
@@ -2,7 +2,7 @@
 import FiliereTable from "@/components/table/FiliereTable";
 import NoteTable from "@/components/table/NoteTable";
 import Semestre from "@/components/table/SemestreTableau";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const semestreData = {
   Semestre1: [
     {
@@ -121,13 +121,76 @@ const semestreData = {
 
 export default function page() {
   const [selectedSemestre, setSelectedSemestre] = useState("");
+  const [notesParSemestre, setNotesParSemestre] = useState<Record<string, Array<{
+    NoteClasse: number;
+    NoteCompo: number;
+    NoteMatiere: number;
+    Validation: string;
+    annee_academique?: string;
+    syllabus?: string;
+    volume_horaire?: number;
+    coefficient?: number;
+    Module: string;
+  }>>>({});
+
+  useEffect(() => {
+    async function fetchStats() {
+      try {
+          // Récupérer les informations de l'utilisateur connecté
+          const userResponse = await fetch("/api/auth/session", {
+            credentials: "include",
+          });
+          const userData = await userResponse.json();
+          const etudiantId = userData.user.etudiant?.id; // Récupérer l'ID de l'étudiant
+          
+        const response = await fetch("/api/utilisateurs/etudiants");
+        const data = await response.json();
+        const etudiants = data.etudiants;
+        const etudiant = etudiants.find(e => e.id === etudiantId);
+          console.log(etudiant);
+          
+        const notesParSemestre = {};
+        
+        etudiant.notes.forEach(note => {
+          const cours = note.cours || {};
+          const semestre = cours.semestre;
+          const filiereModule = cours.filiere_module || {};
+        
+          if (!notesParSemestre[semestre]) {
+            notesParSemestre[semestre] = [];
+          }
+        
+          notesParSemestre[semestre].push({
+            NoteClasse: note.note_class,
+            NoteCompo: note.note_exam,
+            NoteMatiere:note.note_exam,
+            Validation: note.commentaire,
+            annee_academique: cours.sessions?.annee_academique,
+            syllabus: filiereModule.syllabus,
+            volume_horaire: filiereModule.volume_horaire,
+            coefficient: filiereModule.coefficient || 1, // valeur par défaut si pas défini
+            Module: filiereModule.module.nom || 'Module inconnu'
+          });
+        });
+        setNotesParSemestre(notesParSemestre)
+        console.log(notesParSemestre);
+        
+        
+  
+      } catch (error) {
+        console.error("Erreur lors de la récupération des filieres :", error);
+      }
+    }
+  
+    fetchStats();
+  }, []);
   return (
     <div className="ml-4">
       <h1 className="text-2xl font-bold mb-4 ml-2">Les Notes</h1>
       <div className="mt-8 ml-2 mb-4 flex">
         {/* <Semesters /> */}
         <Semestre
-          semestreData={semestreData}
+          semestreData={notesParSemestre}
           setSemestre={setSelectedSemestre}
         />
         {/* tableau */}
@@ -137,7 +200,7 @@ export default function page() {
           ) : (
             <NoteTable
               semesterName={selectedSemestre}
-              data={semestreData[selectedSemestre]}
+              data={notesParSemestre[selectedSemestre]}
             />
           )}
         </div>
